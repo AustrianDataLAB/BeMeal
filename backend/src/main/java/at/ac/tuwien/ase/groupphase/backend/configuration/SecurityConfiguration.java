19f99816a71bac4a502eda71d7aed230fd453d40
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-04-06 15:04:15 +0200

    Use bcrypt instead of scrypt

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/SecurityConfiguration.java
@@ -3,25 +3,24 @@ package at.ac.tuwien.ase.groupphase.backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
-    private static final String[] AUTH_WHITELIST = {"/api/v1/self-service/registration/participant", "/v3/api-docs/**",
-            "/v3/api-docs.yaml", "/swagger-ui/**", "/swagger-ui.html"};
+    private static final String[] AUTH_WHITELIST = { "/api/v1/self-service/registration/participant", "/v3/api-docs/**",
+            "/v3/api-docs.yaml", "/swagger-ui/**", "/swagger-ui.html" };
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeHttpRequests().
-                requestMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated()
-                .and().build();
+        return http.csrf().disable().authorizeHttpRequests().requestMatchers(AUTH_WHITELIST).permitAll().anyRequest()
+                .authenticated().and().build();
     }
 }
