24856d4b0267a0eddddf54a71656b391792685c1
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-04-06 17:33:34 +0200

    Implement token manager

---
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/security/TokenManager.java
@@ -0,0 +1,40 @@
+package at.ac.tuwien.ase.groupphase.backend.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Component
+public class TokenManager {
+
+    private final UserDetailsService userDetailsService;
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    @Autowired
+    public TokenManager(final BeMealUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    public String generateToken(final UserDetails user) {
+        return Jwts.builder().setIssuedAt(Date.from(ZonedDateTime.now().toInstant())).setIssuer("BeMeal")
+                .setSubject(user.getUsername()).setExpiration(Date.from(ZonedDateTime.now().plusWeeks(1).toInstant()))
+                .signWith(secretKey).compact();
+    }
+
+    public UserDetails userDetailsFromToken(final String token) {
+        return userDetailsService.loadUserByUsername(parseToken(token));
+    }
+
+    public String parseToken(final String token) {
+        return Jwts.parserBuilder().setSigningKey(secretKey).build()
+                .parseClaimsJws(token.replace(JwtAuthenticationFilter.BEARER_PREFIX, "")).getBody().getSubject();
+    }
+}
