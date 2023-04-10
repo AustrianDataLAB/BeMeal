87e0401b22fffff5e62695778ae9c78930a95690
Benjamin Probst <benjamin.probst@gmx.at>
2023-04-10 13:38:44 +0200

    added component for creating a league

---
+++ frontend/src/app/components/create-league/create-league.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-league',
+  templateUrl: './create-league.component.html',
+  styleUrls: ['./create-league.component.scss']
+})
+export class CreateLeagueComponent {
+
+}
