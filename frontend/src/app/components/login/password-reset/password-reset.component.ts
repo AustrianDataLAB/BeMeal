ddfd5233d2c9630d490cccda14d277e6e5b41bf7
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-04-11 17:07:37 +0200

    Add reset page

--- frontend/src/app/components/login/password-reset/password-reset.component.ts
+++ frontend/src/app/components/login/password-reset/password-reset.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-password-reset',
-  templateUrl: './password-reset.component.html',
-  styleUrls: ['./password-reset.component.scss']
+    selector: 'app-password-reset',
+    templateUrl: './password-reset.component.html',
+    styleUrls: ['./password-reset.component.scss']
 })
 export class PasswordResetComponent {
 
     private _passwordResetToken: string | null = null;
+
+    tokenRequestForm: FormGroup;
+    passwordResetForm: FormGroup;
+    faulty = false;
+    emailSuccess = false;
+    resetSuccess = false;
 
     get passwordResetToken() {
         return this._passwordResetToken;
@@ -16,5 +23,13 @@ export class PasswordResetComponent {
 
     constructor(private route: ActivatedRoute) {
         this.route.paramMap.subscribe(value => this._passwordResetToken = value.get('token'));
+    }
+
+    requestToken() {
+        console.debug('request token');
+    }
+
+    resetPassword() {
+        console.debug('reset password');
     }
 }
