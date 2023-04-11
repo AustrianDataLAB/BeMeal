5142dd90671ba5ba5f861e585399d00eb534a4da
Benjamin Probst <benjamin.probst@gmx.at>
2023-04-11 23:43:41 +0200

    fixed backend query for getting all leauges

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/LeagueService.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/LeagueService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 @Service
@@ -37,6 +38,6 @@ public class LeagueService {
         Participant user = (Participant) this.userRepository.findByUsername(username);
         List<Participant> participantList = new ArrayList<>();
         participantList.add(user);
-        return this.leagueRepository.findByParticipants(participantList);
+        return this.leagueRepository.findLeaguesByParticipantsIn(new HashSet<>(participantList));
     }
 }
