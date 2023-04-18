d1e644281f8ff2283c86f4db2cffd90e1e3c78c3
Manuel <manuel-waibel@gmx.at>
2023-04-18 17:36:31 +0200

    Fixed formatting

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/datagenerator/BigDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/datagenerator/BigDataGenerator.java
@@ -15,20 +15,20 @@ import java.sql.SQLException;
 @Configuration
 @Profile("BigDataGenerator")
 public class BigDataGenerator {
-	private final Logger logger = LoggerFactory.getLogger(SmallDataGenerator.class);
-	private final DataSource source;
+    private final Logger logger = LoggerFactory.getLogger(SmallDataGenerator.class);
+    private final DataSource source;
 
-	public BigDataGenerator(DataSource source) {
-		this.source = source;
-	}
+    public BigDataGenerator(DataSource source) {
+        this.source = source;
+    }
 
-	@PostConstruct
-	void insertDummyData() {
-		try (Connection c = source.getConnection()) {
-			ScriptUtils.executeSqlScript(c, new ClassPathResource("sql/DefaultDataGen.sql"));
-			ScriptUtils.executeSqlScript(c, new ClassPathResource("sql/BigDataGen.sql"));
-		} catch (SQLException sqle) {
-			logger.error("An error occurred whilst trying to insert testdata", sqle);
-		}
-	}
+    @PostConstruct
+    void insertDummyData() {
+        try (Connection c = source.getConnection()) {
+            ScriptUtils.executeSqlScript(c, new ClassPathResource("sql/DefaultDataGen.sql"));
+            ScriptUtils.executeSqlScript(c, new ClassPathResource("sql/BigDataGen.sql"));
+        } catch (SQLException sqle) {
+            logger.error("An error occurred whilst trying to insert testdata", sqle);
+        }
+    }
 }
