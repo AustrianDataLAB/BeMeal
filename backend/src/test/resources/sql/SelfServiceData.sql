9c0ed7ce25ce874bc85a7c17f8d660fa1a77153e
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2023-04-23 20:41:09 +0200

    remove deletes

--- backend/src/test/resources/sql/SelfServiceData.sql
+++ backend/src/test/resources/sql/SelfServiceData.sql
@@ -1,4 +1,2 @@
-DELETE FROM Participant;
-DELETE FROM Platform_User;
 INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (555, 'John@gmail.com', 'John', '243261243130244537706a65396c31666d496e39516f4e57336f6f4d65687178754d78595a352e4343324339327866725554495246414b2e47654a75', false);
 INSERT INTO Participant (participant_id, postal_code, region, registered, wins) VALUES (555, '1010', 8, '2022-05-26 05:04:49', 52);
