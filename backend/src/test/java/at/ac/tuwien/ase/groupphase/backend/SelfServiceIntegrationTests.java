dc0461c49600be9b9b78fc82f79b3e602cb7d09e
Benjamin Probst <benjamin.probst@gmx.at>
2023-04-23 13:50:18 +0200

    Added integration tests for SelfService

--- backend/src/test/java/at/ac/tuwien/ase/groupphase/backend/SelfServiceIntegrationTests.java
+++ backend/src/test/java/at/ac/tuwien/ase/groupphase/backend/SelfServiceIntegrationTests.java
@@ -1,51 +1,65 @@
 package at.ac.tuwien.ase.groupphase.backend;
 
-import at.ac.tuwien.ase.groupphase.backend.controller.SelfService;
 import at.ac.tuwien.ase.groupphase.backend.dto.Registration;
-import at.ac.tuwien.ase.groupphase.backend.entity.Region;
+import at.ac.tuwien.ase.groupphase.backend.entity.Participant;
+import at.ac.tuwien.ase.groupphase.backend.repository.ParticipantRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import util.Constants;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 public class SelfServiceIntegrationTests {
 
     @Autowired
     private WebTestClient webTestClient;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void registerPlatformUserShouldReturn201() throws Exception {
-        Registration reg = new Registration("test1@gmail.com", "fran1z", "12345678", Region.STYRIA, "2340");
+        Registration reg = new Registration(Constants.VALID_USER_EMAIL, Constants.VALID_USER_USERNAME,
+                Constants.VALID_USER_PASSWORD, Constants.VALID_USER_REGION, Constants.VALID_USER_POSTAL_CODE);
 
         String json = this.objectMapper.writeValueAsString(reg);
         this.webTestClient.post().uri("/api/v1/self-service/registration/participant")
                 .contentType(MediaType.APPLICATION_JSON).bodyValue(json).exchange().expectStatus().isCreated();
-        // mockMvc.perform(post("/api/v1/self-service/registration/participant").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isCreated());
+
+        List<Participant> list1 = new ArrayList<>();
+        this.participantRepository.findAll().forEach(list1::add);
+        assertEquals(1, list1.size());
     }
 
     @Test
-    void registerPlatformUserShouldReturn2011() throws Exception {
-        Registration reg = new Registration("test1@gmail.com", "fran1z", "12345678", Region.STYRIA, "2340");
+    @Sql({ "classpath:sql/SelfServiceData.sql" })
+    void registerPlatformUserTwiceShouldReturn409() throws Exception {
+        Registration reg = new Registration(Constants.EXISTING_USER_EMAIL, Constants.EXISTING_USER_USERNAME,
+                Constants.EXISTING_USER_PASSWORD, Constants.EXISTING_USER_REGION, Constants.EXISTING_USER_POSTAL_CODE);
 
         String json = this.objectMapper.writeValueAsString(reg);
         this.webTestClient.post().uri("/api/v1/self-service/registration/participant")
-                .contentType(MediaType.APPLICATION_JSON).bodyValue(json).exchange().expectStatus().isCreated();
-        // mockMvc.perform(post("/api/v1/self-service/registration/participant").contentType(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isCreated());
+                .contentType(MediaType.APPLICATION_JSON).bodyValue(json).exchange().expectStatus().is4xxClientError();
     }
+
+    // email valideren noch im backend?
+    // @Test
+    // void registerPlatformUserWithInalidEmailShouldReturn201() throws Exception {
+    // Registration reg = new Registration(Constants.INVALID_EMAIL, Constants.VALID_USER_USERNAME,
+    // Constants.VALID_USER_PASSWORD, Constants.VALID_USER_REGION, Constants.VALID_USER_POSTAL_CODE);
+    //
+    // String json = this.objectMapper.writeValueAsString(reg);
+    // this.webTestClient.post().uri("/api/v1/self-service/registration/participant")
+    // .contentType(MediaType.APPLICATION_JSON).bodyValue(json).exchange().expectStatus().is4xxClientError();
+    // }
 
 }
