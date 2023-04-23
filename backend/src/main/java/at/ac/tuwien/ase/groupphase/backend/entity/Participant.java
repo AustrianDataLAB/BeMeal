338a9626f115ef5375db132e114ffe4043df7195
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2023-04-23 17:51:44 +0200

    refactor tests

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/entity/Participant.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/entity/Participant.java
@@ -1,8 +1,10 @@
 package at.ac.tuwien.ase.groupphase.backend.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +13,21 @@ import java.util.List;
 @PrimaryKeyJoinColumn(name = "participant_id")
 @Data
 @EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Participant extends PlatformUser {
+
+    public Participant(Long id, String email, byte[] password, String username, Boolean isAdmin, List<League> ownerOf, String postalCode, Integer wins, Region region, LocalDateTime registered, List<Submission> submissions, List<Submission> votes, List<League> leagues) {
+        super(id, email, password, username, isAdmin, ownerOf);
+        this.postalCode = postalCode;
+        this.wins = wins;
+        this.region = region;
+        this.registered = registered;
+        this.submissions = submissions;
+        this.votes = votes;
+        this.leagues = leagues;
+    }
+
     @Column(nullable = false)
     private String postalCode;
     @Column(nullable = false)
