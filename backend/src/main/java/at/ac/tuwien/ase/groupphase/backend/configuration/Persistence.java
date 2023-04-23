63b10cf5b23e40e719ceeffd8cc658954e530e78
Benjamin Probst <benjamin.probst@gmx.at>
2023-04-23 13:51:09 +0200

    FU

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/Persistence.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/Persistence.java
@@ -1,4 +1,4 @@
-package at.ac.tuwien.ase.groupphase.backend;
+package at.ac.tuwien.ase.groupphase.backend.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -17,29 +17,19 @@ public class Persistence {
     @Autowired
     Environment env;
 
-    @Bean
+    // @Bean
     DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        // dataSource.setUrl(env.getProperty("datasource.url"));
-        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"); // TODO den path zum file ändren, schaut so aus als
-                                                                 // würde er das falsche yml verwenden und dann passt
-                                                                 // die url ned
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        // dataSource.setUrl(env.getProperty("datasource.driver-class-name"));
         dataSource.setUsername(env.getProperty("datasource.user"));
         dataSource.setPassword(env.getProperty("datasource.password"));
 
         return dataSource;
-
-        // final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        // dataSource.setDriverClassName("org.h2.Driver");
-        // dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
-        // dataSource.setUsername("admin");
-        // dataSource.setPassword("password");
-        //
-        // return dataSource;
     }
 
-    @Bean
+    // @Bean
     PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
