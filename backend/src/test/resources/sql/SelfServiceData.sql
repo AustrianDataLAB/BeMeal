ed225c17a09774ba6cbf4ca185d31728a6e09bbb
Benjamin Probst <benjamin.probst@gmx.at>
2023-04-23 20:33:24 +0200

    Pull yourself together pipe!

--- backend/src/test/resources/sql/SelfServiceData.sql
+++ backend/src/test/resources/sql/SelfServiceData.sql
@@ -1,3 +1,4 @@
+DELETE FROM League_Participants;
 DELETE FROM Participant;
 DELETE FROM Platform_User;
 INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (555, 'John@gmail.com', 'John', '243261243130244537706a65396c31666d496e39516f4e57336f6f4d65687178754d78595a352e4343324339327866725554495246414b2e47654a75', false);
