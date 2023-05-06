bc683c75ba43abc03c6e36cb7160238cbf4a8fbb
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-05-06 22:34:45 +0200

    Implement last winner in backend

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/dto/LeagueDto.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/dto/LeagueDto.java
@@ -3,6 +3,9 @@ package at.ac.tuwien.ase.groupphase.backend.dto;
 import at.ac.tuwien.ase.groupphase.backend.entity.GameMode;
 import at.ac.tuwien.ase.groupphase.backend.entity.Region;
 
-public record LeagueDto(Long id, String name, GameMode gameMode, Integer challengeDuration, Region region) {
+import java.util.List;
+
+public record LeagueDto(Long id, String name, GameMode gameMode, Integer challengeDuration, Region region,
+        List<String> lastWinners) {
 
 }
--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/repository/LeagueRepository.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/repository/LeagueRepository.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.ase.groupphase.backend.repository;
 
+import at.ac.tuwien.ase.groupphase.backend.entity.Challenge;
 import at.ac.tuwien.ase.groupphase.backend.entity.League;
 import at.ac.tuwien.ase.groupphase.backend.entity.Participant;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -23,4 +25,7 @@ public interface LeagueRepository extends CrudRepository<League, Long> {
 
     @Query("select distinct l from League l left join l.challenges c where c is null")
     Stream<League> findLeaguesWithNoChallenges();
+
+    @Query("select distinct c from League l left join l.challenges c where l.id = :leagueId and c.endDate < :endExclusive order by c.endDate desc limit 1")
+    Optional<Challenge> findLastEndedChallenge(long leagueId, LocalDate endExclusive);
 }
