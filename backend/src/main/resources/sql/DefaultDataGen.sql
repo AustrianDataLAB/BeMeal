18378a888968dfe301b2dad254d84faffb075415
Matteo Kofler <e11904672@student.tuwien.ac.at>
2023-05-08 14:28:49 +0200

    couldnt register new user when db started with datagen -> fixed smalldatagen

--- backend/src/main/resources/sql/DefaultDataGen.sql
+++ backend/src/main/resources/sql/DefaultDataGen.sql
@@ -1,17 +1,17 @@
 /*      Create users        */
 -- Admin accounts
 -- Passwords are encrypted matriculation IDs
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (1, 'benjamin.probst@bemeal.at', 'Bini', '$2a$12$nsiO.0j8gKPbbLssvXxR5.F6NuIX1BAb5Ll14LH.6MJHjxKuVvI/O', true);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (2, 'dave.pfliegler@bemeal.at', 'Dave', '$2a$12$37Oc6dzwqWXR2Jn1mkCs.uLOPkzpBUdj8M0bHtwYybBwSG.7Gj/OG', true);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (3, 'dennis.toth@bemeal.at', 'Dennis', '$2a$10$DFYTkpABc.pTpJWCYMBvW.Wijxwl3lr67G6I5r5Pzcr.5/p2V4xwm', true);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (4, 'manuel.waibel@bemeal.at', 'Manu', '$2a$10$TOPbeItKkxyC2SAIw6DXN.y725xGXG5Rxs8ZG6JaaZU..gDCU.DWy', true);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (5, 'matteo.kofler@bemeal.at', 'Matteo', '$2a$10$p61cLtBQ3h3vwXy2GB9c.OJPX9ae3qp8SLNNmCxt0mZXPd.rWOicG', true);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (6, 'richard.stoeckl@bemeal.at', 'Richi', '$2a$10$diB3.0ugwJk.7wdeAY0PBOU90Ukw4MQtOAvbMxnO.tsE7mtRsLv5C', true);
+INSERT INTO Platform_User (email, username, password, is_admin) VALUES ( 'benjamin.probst@bemeal.at', 'Bini', '$2a$12$nsiO.0j8gKPbbLssvXxR5.F6NuIX1BAb5Ll14LH.6MJHjxKuVvI/O', true);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ('dave.pfliegler@bemeal.at', 'Dave', '$2a$12$37Oc6dzwqWXR2Jn1mkCs.uLOPkzpBUdj8M0bHtwYybBwSG.7Gj/OG', true);
+INSERT INTO Platform_User (email, username, password, is_admin) VALUES ('dennis.toth@bemeal.at', 'Dennis', '$2a$10$DFYTkpABc.pTpJWCYMBvW.Wijxwl3lr67G6I5r5Pzcr.5/p2V4xwm', true);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ( 'manuel.waibel@bemeal.at', 'Manu', '$2a$10$TOPbeItKkxyC2SAIw6DXN.y725xGXG5Rxs8ZG6JaaZU..gDCU.DWy', true);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ('matteo.kofler@bemeal.at', 'Matteo', '$2a$10$p61cLtBQ3h3vwXy2GB9c.OJPX9ae3qp8SLNNmCxt0mZXPd.rWOicG', true);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ( 'richard.stoeckl@bemeal.at', 'Richi', '$2a$10$diB3.0ugwJk.7wdeAY0PBOU90Ukw4MQtOAvbMxnO.tsE7mtRsLv5C', true);
 -- Show user accounts
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (7, 'test1@gmail.com', 'test1', '$2a$10$qlE9fTpRBEsw1nBpFXvcPOPafSnlKKIfSNHMceKizTGdckSU1qbJi', false);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (8, 'test2@gmail.com', 'test2', '$2a$10$2CUt/N6l99GmxWmfBBXnDOTMueJDFOhKUdQDUyk7NQpxX4gmDVPpO', false);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (9, 'test3@gmail.com', 'test3', '$2a$10$sx/9lMzt/9s9QG.NvtvHf.JbTdAqmRahMEjuNcAqOp47nO6uH.v0G', false);
-INSERT INTO Platform_User (id, email, username, password, is_admin) VALUES (10, 'test4@gmail.com', 'test4', '$2a$10$3QSmui3.ul8jAWpXRzKw4eqSrO7822dM5qIecTFOYG4GHwXfHSNuO', false);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ( 'test1@gmail.com', 'test1', '$2a$10$qlE9fTpRBEsw1nBpFXvcPOPafSnlKKIfSNHMceKizTGdckSU1qbJi', false);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ( 'test2@gmail.com', 'test2', '$2a$10$2CUt/N6l99GmxWmfBBXnDOTMueJDFOhKUdQDUyk7NQpxX4gmDVPpO', false);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ('test3@gmail.com', 'test3', '$2a$10$sx/9lMzt/9s9QG.NvtvHf.JbTdAqmRahMEjuNcAqOp47nO6uH.v0G', false);
+INSERT INTO Platform_User ( email, username, password, is_admin) VALUES ( 'test4@gmail.com', 'test4', '$2a$10$3QSmui3.ul8jAWpXRzKw4eqSrO7822dM5qIecTFOYG4GHwXfHSNuO', false);
 
 /*      Create show user Participants     */
 INSERT INTO Participant (participant_id, postal_code, region, registered, wins) VALUES (7, 1100, 1, '2021-01-22 16:38:54', 66);
