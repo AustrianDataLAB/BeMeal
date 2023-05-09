e557a68c00096ad0d55acf6ccc7a8846d93a20a3
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-05-09 20:30:08 +0200

    Migrate legacy upvotes to new system

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/mapper/LeagueMapper.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/mapper/LeagueMapper.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.ase.groupphase.backend.mapper;
 
 import at.ac.tuwien.ase.groupphase.backend.dto.LeagueDto;
 import at.ac.tuwien.ase.groupphase.backend.entity.League;
+import at.ac.tuwien.ase.groupphase.backend.entity.ParticipantSubmissionVote;
 import at.ac.tuwien.ase.groupphase.backend.entity.Submission;
 import at.ac.tuwien.ase.groupphase.backend.repository.LeagueRepository;
 import jakarta.validation.constraints.NotNull;
@@ -33,13 +34,15 @@ public class LeagueMapper {
         final var lastChallenge = this.leagueRepository.findLastEndedChallenge(league.getId(), LocalDate.now());
         final var lastWinners = lastChallenge.map(c -> {
             final var orderedSubmissions = c
-                    .getSubmissions().stream().sorted(Comparator
-                            .comparingInt((Submission submission) -> submission.getUpVotes().size()).reversed())
+                    .getSubmissions().stream().sorted(Comparator.comparingLong((Submission submission) -> submission
+                            .getUpVotes().stream().filter(ParticipantSubmissionVote::isUpvote).count()).reversed())
                     .toList();
-            final var maxUpVotes = orderedSubmissions.stream().findFirst().map(s -> s.getUpVotes().size())
-                    .orElse(Integer.MAX_VALUE);
+            final var maxUpVotes = orderedSubmissions.stream().findFirst()
+                    .map(s -> s.getUpVotes().stream().filter(ParticipantSubmissionVote::isUpvote).count())
+                    .orElse(Long.MAX_VALUE);
             return orderedSubmissions.stream()
-                    .filter(s -> s.getUpVotes().size() == maxUpVotes && !s.getUpVotes().isEmpty())
+                    .filter(s -> s.getUpVotes().stream().filter(ParticipantSubmissionVote::isUpvote)
+                            .count() == maxUpVotes && !s.getUpVotes().isEmpty())
                     .map(s -> s.getParticipant().getUsername()).toList();
         });
         return new LeagueDto(league.getId(), league.getName(), league.getGameMode(), league.getChallengeDuration(),
