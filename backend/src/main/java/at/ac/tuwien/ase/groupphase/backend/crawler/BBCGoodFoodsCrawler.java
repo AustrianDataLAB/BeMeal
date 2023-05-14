c30577402aed8d92e72b10248d06f946eecc1042
Dennis Toth <e11907577@student.tuwien.ac.at>
2023-05-14 23:02:59 +0200

    #54 Move picture crawler to own package

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/crawler/BBCGoodFoodsCrawler.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/crawler/BBCGoodFoodsCrawler.java
@@ -1,4 +1,4 @@
-package at.ac.tuwien.ase.groupphase.backend;
+package at.ac.tuwien.ase.groupphase.backend.crawler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
