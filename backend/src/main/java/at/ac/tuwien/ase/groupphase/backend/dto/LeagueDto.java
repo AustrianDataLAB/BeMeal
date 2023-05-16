21841cce5e320f1847b2879639139be8f2c6cf8f
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2023-05-16 17:43:04 +0200

    format backend

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/dto/LeagueDto.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/dto/LeagueDto.java
@@ -4,7 +4,6 @@ import at.ac.tuwien.ase.groupphase.backend.entity.GameMode;
 
 import java.util.List;
 
-public record LeagueDto(Long id, String name, GameMode gameMode, Integer challengeDuration,
-                        List<String> lastWinners) {
+public record LeagueDto(Long id, String name, GameMode gameMode, Integer challengeDuration, List<String> lastWinners) {
 
 }
