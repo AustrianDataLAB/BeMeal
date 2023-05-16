2c1d6e46a0e9f36434e0bc271cdfde313bea9524
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-05-16 23:03:55 +0200

    #89, Reference div container to vega embed

--- frontend/src/app/components/analytics/heat-map/heat-map.component.ts
+++ frontend/src/app/components/analytics/heat-map/heat-map.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {StatisticsService} from '../../../services/statistics.service';
+import {HeatMap, HeatMapTye} from '../../../dtos/statistics';
+import embed, {VisualizationSpec} from 'vega-embed';
 
 @Component({
-  selector: 'app-heat-map',
-  templateUrl: './heat-map.component.html',
-  styleUrls: ['./heat-map.component.scss']
+    selector: 'app-heat-map', templateUrl: './heat-map.component.html', styleUrls: ['./heat-map.component.scss']
 })
 export class HeatMapComponent {
 
+    @ViewChild('heatMapContainer') heatMapContainer: ElementRef;
+
+    private heatMap: HeatMap;
+
+    constructor(private statisticsService: StatisticsService) {
+        statisticsService.getHeatMap(HeatMapTye.RANDOM, false).subscribe({
+            next: value => {
+                this.heatMap = value;
+                embed(this.heatMapContainer.nativeElement, this.heatMapSpec()).then(r => console.debug(r));
+            }, error: err => {
+                console.error(err);
+            }
+        });
+    }
+
+    private heatMapSpec(): VisualizationSpec {
+        return {
+            $schema: 'https://vega.github.io/schema/vega-lite/v5.json', data: {
+                url: '', format: {
+                    type: 'topojson', feature: 'gemeinden'
+                }
+            }, mark: {
+                type: 'geoshape', stroke: 'white'
+            }, width: 1000, height: 1000
+        };
+    }
 }
