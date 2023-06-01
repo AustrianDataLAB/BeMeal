ea9da5522c2e3083673ac134d9317156f9e75451
Manuel <manuel-waibel@gmx.at>
2023-06-01 20:06:31 +0200

    #72, #43 Wins update: Updated javadoc

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/ChallengeGenerationService.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/ChallengeGenerationService.java
@@ -69,6 +69,7 @@ public class ChallengeGenerationService {
 
     /**
      * Generate new challenges for all leagues which either have no or only expired challenges.
+     * Also updates wins of Participants, if they have won the previous expired challenge
      */
     @Transactional
     public void generateForExpiredChallenges() {
