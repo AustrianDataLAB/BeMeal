d6375ca7af59f2b6b914624d8dd42b8473421328
Dave <80040393+DaveWasTakn@users.noreply.github.com>
2023-06-12 17:30:40 +0200

    #116 extend fixNeo4jData.py script

--- backend/database/neo4j/fixNeo4jData.py
+++ backend/database/neo4j/fixNeo4jData.py
@@ -1,8 +1,18 @@
+import os
+import json
+
 ############################################################################
 neo4jData = "./data_old.json" # input with unicode artefacts
 newFile = "./data.json"       # fixed output
+picturesLocation = "../../src/main/resources/recipes"
 ############################################################################
 
+pictures = [x.split(".")[0] for x in os.listdir(picturesLocation) if os.path.isfile(os.path.join(picturesLocation, x))]
+
+def isCorrectEntry(x: str):
+    return ("null" not in x) and (json.loads(x)["page"]["recipe"]["picture_uuid"] in pictures)
+
+################## fix encoding artefacts ##################
 with open(neo4jData, 'r', encoding='utf-8') as file:
     data: str = file.read()
     data = data.replace(u"\u00A0", " ")\
@@ -15,5 +25,9 @@ with open(neo4jData, 'r', encoding='utf-8') as file:
         .encode('latin-1', errors='ignore')\
         .decode('utf-8', errors='ignore')
     f = open(newFile, "w", encoding='utf-8')
-    f.write(data)
+
+    ########## remove corrupted data ##########
+    correctEntries = [x for x in data.split("\n") if isCorrectEntry(x)]
+
+    f.write("\n".join(correctEntries))
     f.close()
