e900acb09155f3f4495898ae88d01dfebd4a7a2f
Benjamin Probst <benjamin.probst@gmx.at>
2023-06-18 17:21:29 +0200

    #111 fixed pipe

--- frontend/src/app/dtos/profile.ts
+++ frontend/src/app/dtos/profile.ts
@@ -7,6 +7,7 @@ export class Profile {
         public username: string,
         public email: string,
         public postalCode: number,
+        // eslint-disable-next-line @typescript-eslint/ban-types
         public wins: Object,
         public region: Region,
         public registered: string,
