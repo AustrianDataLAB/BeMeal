71b8a61cc167c97e5be3c8f0edf426e970c0d30f
Manuel <manuel-waibel@gmx.at>
2023-06-19 19:45:58 +0200

    #43 Refactoring: refactored Password reset to match architecture

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/SelfService.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/service/SelfService.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.ase.groupphase.backend.service;
 
+import at.ac.tuwien.ase.groupphase.backend.dto.PasswordResetDto;
 import at.ac.tuwien.ase.groupphase.backend.endpoint.UserEndpoint;
 import at.ac.tuwien.ase.groupphase.backend.dto.RegistrationDto;
 import at.ac.tuwien.ase.groupphase.backend.exception.UserAlreadyExistsException;
@@ -11,7 +12,10 @@ import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +27,7 @@ public class SelfService {
     private final RegistrationMapper registrationMapper;
     private final PostalCodeValidator postalCodeValidator;
     private final LeagueService leagueService;
+    private final PasswordEncoder passwordEncoder;
 
     public void register(RegistrationDto registrationDto) throws ValidationException {
         if (this.userRepository.exists(registrationDto.email(), registrationDto.password())) {
@@ -34,10 +39,38 @@ public class SelfService {
         }
         final var participant = this.registrationMapper.registrationToParticipant(registrationDto);
         this.participantRepository.save(participant);
-        logger.info("Registered participant with id: '{}' email: '{}' username: '{}'", participant.getId(),
+        logger.trace("Registered participant with id: '{}' email: '{}' username: '{}'", participant.getId(),
                 participant.getEmail(), participant.getUsername());
         // add user to regional league:
         this.leagueService.joinRegionalLeague(participant.getUsername(), participant.getRegion());
+    }
+
+    public UUID getPasswordResetTokenForUser(String email) {
+        final var user = this.userRepository.findByEmail(email);
+        if (user == null) {
+            logger.warn("No user with email '{}' exists, do not send an email", email);
+            return null;
+        }
+        final var passwordResetToken = UUID.randomUUID();
+        user.setPasswordResetToken(passwordResetToken);
+        this.userRepository.save(user);
+
+        logger.trace("Generated password reset token for user '{}'", user.getUsername());
+
+        return passwordResetToken;
+    }
+
+    public void resetPassword(UUID passwordResetToken, PasswordResetDto passwordResetDto) {
+        final var user = this.userRepository.findByPasswordResetToken(passwordResetToken);
+        if (user == null) {
+            logger.warn("No user which currently is registered to the reset token '{}'", passwordResetToken);
+            return;
+        }
+        user.setPasswordResetToken(null);
+        user.setPassword(this.passwordEncoder.encode(passwordResetDto.password()).getBytes());
+        this.userRepository.save(user);
+
+        logger.trace("Updated password for user '{}'", user.getUsername());
     }
 
 }
