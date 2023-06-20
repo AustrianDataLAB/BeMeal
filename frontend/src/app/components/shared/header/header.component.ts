a7c0af913c3c4f8245642e115cee33d9ca4e0c49
Richard Stöckl <e11908080@student.tuwien.ac.at>
2023-06-20 16:26:37 +0200

    #121, Disable analytics for participants

--- frontend/src/app/components/shared/header/header.component.ts
+++ frontend/src/app/components/shared/header/header.component.ts
@@ -3,13 +3,23 @@ import {SelfService} from "../../../services/self.service";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header', templateUrl: './header.component.html', styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+    private isParticipant = true;
+
     constructor(private selfService: SelfService, private router: Router) {
+        this.selfService.getProfile().subscribe({
+            next: () => console.debug('user is a participant'), error: () => {
+                console.debug('user is a gamemaster');
+                this.isParticipant = false;
+            }
+        });
+    }
+
+    get viewAnalytics(): boolean {
+        return !this.isParticipant || localStorage.getItem('bemeal.menu.showanalytics') != null;
     }
 
     logout() {
