607079d42aaee8078871958fd419384155cbdb56
Nikolaus Walther <1459063+nwpr@users.noreply.github.com>
2024-04-12 16:28:50 +0200

    added BACKEND_URL environment variable

---
+++ frontend/docker/read-environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Define the output JSON file
+output_file="/bemeal-frontend/env.json"
+
+# Check if the environment variable ABC is set
+if [ -z "$BACKEND_URL" ]; then
+    echo "Environment variable BACKEND_URL is not set."
+    exit 1
+fi
+
+# Create a JSON object with the environment variable ABC
+echo "{ \"BACKEND_URL\": \"$BACKEND_URL\" }" > "$output_file"
+
+echo "BACKEND_URL has been copied to $output_file."
