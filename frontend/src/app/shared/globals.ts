b8096e71764487677be705483c4e0662230d2087
sasha <ku3menko.sasha@gmail.com>
2024-05-05 21:22:45 +0200

    always use configured env

--- frontend/src/app/shared/globals.ts
+++ frontend/src/app/shared/globals.ts
@@ -1,29 +1,11 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {environment} from "@environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class Globals {
-    private _backendUri: string;
-    private httpClient = inject(HttpClient);
-
-    get backendUri(): string {
-        if (this._backendUri!!) {
-            return this._backendUri;
-        }
-        this.httpClient.get<{ BACKEND_URL: string }>('/assets/env.json').subscribe({
-            next: urlVar => this._backendUri = urlVar.BACKEND_URL,
-            error: error => {
-                if (window.location.port === '4200') { // local `ng serve`, backend at localhost:8080
-                    this._backendUri = 'http://localhost:8080/api/v1';
-                } else {
-                    console.error(`Could not load environment variables ${error}`);
-                }
-            }
-        });
-        return this._backendUri;
-    }
+    readonly backendUri = environment.backendUri
 }
 
 
