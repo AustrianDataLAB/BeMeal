c121eb30adaed61be81ff631332938329d94bcea
sasha <ku3menko.sasha@gmail.com>
2024-05-27 23:33:00 +0200

    cleanup and format

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/Persistence.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/Persistence.java
@@ -1,14 +1,10 @@
 package at.ac.tuwien.ase.groupphase.backend.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -28,9 +24,9 @@ public class Persistence {
         return dataSource;
     }
 
-//    @Bean
-//    @Primary
-//    PlatformTransactionManager transactionManager() {
-//        return new DataSourceTransactionManager(dataSource());
-//    }
+    // @Bean
+    // @Primary
+    // PlatformTransactionManager transactionManager() {
+    // return new DataSourceTransactionManager(dataSource());
+    // }
 }
