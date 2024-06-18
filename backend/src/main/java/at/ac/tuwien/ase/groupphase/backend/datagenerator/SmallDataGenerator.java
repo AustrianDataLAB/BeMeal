19dfbeb617a4300c99e3805b40882bc7984305b6
sasha <ku3menko.sasha@gmail.com>
2024-06-18 17:52:48 +0200

    make sure rdbmDataSource is used

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/datagenerator/SmallDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/datagenerator/SmallDataGenerator.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
@@ -26,7 +27,7 @@ public class SmallDataGenerator {
     @Autowired
     private CommunityIdentificationService communityIdentificationService;
 
-    public SmallDataGenerator(DataSource source) {
+    public SmallDataGenerator(@Qualifier("rdbmsDataSource") DataSource source) {
         this.source = source;
     }
 
