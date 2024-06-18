66c06baf1eb430cd462a9990a4faecdf10141cee
sasha <ku3menko.sasha@gmail.com>
2024-06-18 19:04:47 +0200

    wait till league is loaded on page

--- test/tests/main.spec.ts
+++ test/tests/main.spec.ts
@@ -15,18 +15,14 @@ test.describe.serial('Leagues page test', () => {
         await page.getByTestId("login-button").click()
 
         await expect(page).toHaveURL(`${url}/leagues`);
-        await page.content().then(value => console.log(value));
-        const leagueExists = await page.$('[data-testid="league-name-0"]') !== null;
-        // the code  here got bitten and might be written cleaner
-        if (leagueExists) {
-            const gameMode = await page.textContent('[data-testid="league-gamemode-0"]');
-            const challengeDuration = await page.textContent('[data-testid="league-challenge-duration-0"]');
 
-            if (gameMode !== 'Gamemode: Picture Ingredients' || challengeDuration !== 'Challenge duration: 7 days') {
-                throw new Error('League details are incorrect. Please check the values.');
-            }
-        } else {
-            throw new Error('The league "Uni friends" does not exist. Please check the league name.');
+        await expect(page.getByTestId("league-name-0")).toBeVisible();
+        // the code  here got bitten and might be written cleaner
+        const gameMode = await page.textContent('[data-testid="league-gamemode-0"]');
+        const challengeDuration = await page.textContent('[data-testid="league-challenge-duration-0"]');
+
+        if (gameMode !== 'Gamemode: Picture Ingredients' || challengeDuration !== 'Challenge duration: 7 days') {
+            throw new Error('League details are incorrect. Please check the values.');
         }
     });
 })
