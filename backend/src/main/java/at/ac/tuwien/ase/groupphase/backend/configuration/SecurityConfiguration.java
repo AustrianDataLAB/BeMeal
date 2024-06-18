50e4cb08ed00644fddaa8bfb97f6efcf74680bef
sasha <ku3menko.sasha@gmail.com>
2024-06-18 17:23:23 +0200

    include import

--- backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/at/ac/tuwien/ase/groupphase/backend/configuration/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
